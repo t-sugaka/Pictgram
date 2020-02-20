3484fc1c774a9a2d99fed9f99443cd71e4e32b38
EC2 Default User <ec2-user@ip-172-31-27-183.us-east-2.compute.internal>
2020-02-20 03:40:26 +0000

    コンフリクトを起こした箇所の修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: log_in_params[:email])
+    if user && user.authenticate(log_in_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -19,6 +19,10 @@ class SessionsController < ApplicationController
   end
   
   private
+  def log_in_params
+    params.require(:session).permit(:email, :password)
+  end
+  
   def log_in(user)
     session[:user_id] = user.id
   end
