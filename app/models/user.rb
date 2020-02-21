28d8a5592aec7caced4d73d0ad92fdc41e3215ed
EC2 Default User <ec2-user@ip-172-31-27-183.us-east-2.compute.internal>
2020-02-21 15:29:06 +0000

    Rails06 投稿一覧表示まで完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 15}
   validates :email, presence: true,
                     format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
-                    
-  has_secure_password
   validates :password, presence: true, length: {minimum: 8, maximum: 32},
                                        format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+                                       
+  has_secure_password
+  
+  has_many :topics
 end
