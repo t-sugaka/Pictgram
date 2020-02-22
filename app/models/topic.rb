232f77b212b365f1553707894dafcef29e185e81
EC2 Default User <ec2-user@ip-172-31-27-183.us-east-2.compute.internal>
2020-02-22 09:37:08 +0000

    Revert "Rails06 投稿一覧表示まで完了"
    This reverts commit 28d8a5592aec7caced4d73d0ad92fdc41e3215ed.

--- app/models/topic.rb
+++
@@ -1,9 +0,0 @@
-class Topic < ApplicationRecord
-  validates :user_id, presence: true
-  validates :description, presence: true
-  validates :image, presence: true
-  
-  belongs_to :user
-  
-  mount_uploader :image, ImageUploader
-end
